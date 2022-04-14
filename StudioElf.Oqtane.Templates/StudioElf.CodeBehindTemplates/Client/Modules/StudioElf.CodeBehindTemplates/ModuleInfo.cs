56ed6c67d65f90170570b45863689fc55ad9caee
Leigh Pointer <leigh.pointer@outloo.com>
2022-04-14 11:49:15 +0200

    Set to 3.1.0

--- StudioElf.Oqtane.Templates/StudioElf.CodeBehindTemplates/Client/Modules/StudioElf.CodeBehindTemplates/ModuleInfo.cs
+++ StudioElf.Oqtane.Templates/StudioElf.CodeBehindTemplates/Client/Modules/StudioElf.CodeBehindTemplates/ModuleInfo.cs
@@ -9,8 +9,8 @@ namespace StudioElf.CodeBehindTemplates
         {
             Name = "Studio-Elf Code-behind Templates [C#]",
             Description = "Studio-Elf Code-behind Templates [C#]",
-            Version = "1.1.0",
-            ReleaseVersions = "1.0.0,1.1.0",
+            Version = "3.1.0",
+            ReleaseVersions = "1.0.0,1.1.0,3.1.0",
             PackageName = "StudioElf.CodeBehindTemplates" 
         };
     }
